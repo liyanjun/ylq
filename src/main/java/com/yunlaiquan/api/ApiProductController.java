678f6e80bd3ebbc0cf1c5311bb786d487d7fa25a
weichanchan <weicc_selina@163.com>
2018-05-30 22:43:33 +0800

    商品接口API初始化

---
+++ src/main/java/com/yunlaiquan/api/ApiProductController.java
@@ -0,0 +1,43 @@
+package com.yunlaiquan.api;
+
+import com.yunlaiquan.admin.product.service.ProductInfoService;
+import com.yunlaiquan.utils.R;
+import com.yunlaiquan.utils.annotation.IgnoreAuth;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author  weicc
+ * @date  2018/5/30 21:15
+ * @desc
+ **/
+@RestController
+@RequestMapping("/api")
+@Api("商品接口")
+public class ApiProductController {
+    @Autowired
+    private ProductInfoService productInfoService;
+
+    /**
+     * 商品查询
+     */
+    @IgnoreAuth
+    @PostMapping("trackProduct")
+    @ApiOperation(value = "商品查询")
+    @ApiImplicitParams({
+        @ApiImplicitParam(paramType = "query", dataType="string", name = "name", value = "商品名稱", required = true),
+        @ApiImplicitParam(paramType = "query", dataType="double", name = "amount", value = "实价", required = true),
+        @ApiImplicitParam(paramType = "query", dataType="string", name = "brand_name", value = "商品品牌", required = true),
+        @ApiImplicitParam(paramType = "query", dataType="int", name = "bucket_type", value = "桶类型", required = true)
+    })
+    public R trackProduct(String name, double amount, String brand_name, int bucket_type){
+        return R.ok();
+
+    }
+}
