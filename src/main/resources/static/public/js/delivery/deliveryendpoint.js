8f3198cbc32d41891081315aa16446e621dc57c2
weichanchan <weicc_selina@163.com>
2018-06-03 22:33:22 +0800

    配送点地图调整

--- src/main/resources/static/public/js/delivery/deliveryendpoint.js
+++ src/main/resources/static/public/js/delivery/deliveryendpoint.js
@@ -51,6 +51,15 @@ var vm = new Vue({
 			vm.showList = false;
 			vm.title = "新增";
 			vm.deliveryEndpoint = {};
+            window.addEventListener('message', function(event) {
+                // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
+                var loc = event.data;
+                if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
+                    vm.deliveryEndpoint.locationX = loc.latlng.lat;
+                    vm.deliveryEndpoint.locationY = loc.latlng.lng;
+                    // console.log('location', loc);
+                }
+            }, false);
 		},
 		update: function (event) {
 			var id = getSelectedRow();
