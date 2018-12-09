9f87204d6fe0b855fe90a1a7feb0d4d29f9ee37e
liyanjun <280378807@qq.com>
2018-12-09 09:56:27 +0800

    文件上传问题

--- src/main/java/com/yunquanlai/YlqApplication.java
+++ src/main/java/com/yunquanlai/YlqApplication.java
@@ -5,11 +5,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.servlet.MultipartConfigElement;
 
 @EnableAsync
 @EnableCaching
@@ -19,5 +23,15 @@ public class YlqApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YlqApplication.class, args);
+	}
+
+	/**
+	 * 文件上传临时路径
+	 */
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setLocation("/app/tmp");
+		return factory.createMultipartConfig();
 	}
 }
